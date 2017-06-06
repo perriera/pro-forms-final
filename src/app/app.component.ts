4155aaaa64fca07f591cf84c9477b9d6eaf7d842
Perry Anderson <perry.anderson@gmail.com>
2017-06-06 13:57:20 -0300

    Added Firebase and Polyfill support and keys for Firebase Authentication usage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
   loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
+
+  ngOnInit() {
+
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB_R9Tj5hXMTnhOVQwZJ7xnBzcMzMIVZIg',
+      authDomain: 'ng-recipe-book-2ab4d.firebaseapp.com'
+    });
+  }
+
 }
