3fbfe25f0369881572004d9825c6184dc7bfc711
Perry Anderson <perry.anderson@gmail.com>
2017-06-07 20:21:07 -0300

    How to use preloadingStrategy ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 import { AuthGuard } from './auth/auth-guard.service';
 
@@ -7,12 +7,12 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'recipes', loadChildren: './recipes/recipe.module#RecipesModule',
-    canLoad: [ AuthGuard ]
+    // canLoad: [ AuthGuard ]
   }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(appRoutes) ],
+  imports: [ RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {
