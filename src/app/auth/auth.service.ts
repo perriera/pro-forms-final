08b629c51ac232248602191b148c2e472b91dc63
Perry Anderson <perry.anderson@gmail.com>
2017-06-06 22:04:08 -0300

    Authentication implemented for store and retrieve operations.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,9 +3,11 @@ import { Response } from '@angular/http';
 
 export class AuthService {
 
+  token: string;
+
   signupUser(email: string, password: string, resolve, reject) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then( (response: Response) => resolve(response))
+      .then((response: Response) => resolve(response))
       .catch(
         error => reject(error)
       );
@@ -13,10 +15,26 @@ export class AuthService {
 
   signinUser(email: string, password: string, resolve, reject) {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then( (response: Response) => resolve(response))
+      .then((response: Response) => {
+        firebase.auth().currentUser.getToken()
+          .then(
+            (token: string) => {
+              this.token = token;
+            }
+          );
+        resolve(response);
+      })
       .catch(
         error => reject(error)
       );
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getToken()
+      .then(
+        (token: string) => this.token = token
+      );
+    return this.token;
   }
 
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
@@ -11,22 +11,29 @@ export class DataStorageService {
   recipesUrl = 'https://ng-recipe-book-2ab4d.firebaseio.com/recipes.json';
 
   constructor(private http: Http,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
+
+  authUrl() {
+    const token = this.authService.getToken();
+    const url = this.recipesUrl + '?auth=' + token;
+    return url;
   }
 
   storeRecipes() {
-    return this.http.put(this.recipesUrl, this.recipeService.getRecipes());
+    return this.http.put(this.authUrl(), this.recipeService.getRecipes());
   }
 
   fetchRecipes() {
-    this.http.get(this.recipesUrl)
+    this.http.get(this.authUrl())
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
-          for ( const recipe of recipes ) {
-            if ( !recipe['ingredients'] ) {
+          for (const recipe of recipes) {
+            if (!recipe[ 'ingredients' ]) {
               console.log(recipe);
-              recipe['ingredients'] = [];
+              recipe[ 'ingredients' ] = [];
             }
           }
           return recipes;
